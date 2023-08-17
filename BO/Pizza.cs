f3a1fed416be74907fc3af2c00b73d9082fc9c94
Dylamn <dylan.vallee@hotmail.com>
2023-08-17 15:01:43 +0200

    Add 'BO' project

---
+++ BO/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace BO
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public Pate Pate { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}
